3773bb364bb5499f6bc0a39bb548848a09d4a493
DoJeon1 <donghwan.dj@gmail.com>
2021-03-30 01:09:52 -0600

    added song model

--- musicplayer/models.py
+++ musicplayer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    song_title = models.CharField(max_length=200)
+    song_duration = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.song_title
