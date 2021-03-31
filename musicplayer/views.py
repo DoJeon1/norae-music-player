afc9267a05339e82853534a2909da3be29bc4813
DoJeon1 <donghwan.dj@gmail.com>
2021-03-30 23:24:55 -0600

    created a view using template index.html file

--- musicplayer/views.py
+++ musicplayer/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Song
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, you're at the musicplayer index")
+    template = loader.get_template('index.html')
+    context = {
+        'song_list': Song
+    }
+    return HttpResponse(template.render(context, request))
