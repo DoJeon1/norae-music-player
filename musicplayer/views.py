dbca0131986cefa26dc94e8d7978fea398f2974c
DoJeon1 <donghwan.dj@gmail.com>
2021-03-30 23:26:57 -0600

    changed loader to shortcut 'render' format

--- musicplayer/views.py
+++ musicplayer/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
 from .models import Song
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
     context = {
         'song_list': Song
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
