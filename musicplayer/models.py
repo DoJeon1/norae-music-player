bde90d9e312a989577b0fd4031d2a7c24e7e196e
DoJeon1 <donghwan.dj@gmail.com>
2021-03-31 22:40:12 -0600

    added additional fields in the song model for cover image, artist, file source

--- musicplayer/models.py
+++ musicplayer/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Song(models.Model):
     song_title = models.CharField(max_length=200)
+    song_artist = models.CharField(max_length=200)
     song_duration = models.IntegerField(default=0)
+    cover_image = models.ImageField(upload_to='media')
+    song_file = models.FileField(upload_to='media')
 
     def __str__(self):
         return self.song_title
