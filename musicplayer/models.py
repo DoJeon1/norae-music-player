c8497fc223f52896752b5a1a60fc480ae0c51df2
DoJeon1 <donghwan.dj@gmail.com>
2021-03-31 22:51:48 -0600

    changed field type for song duration

--- musicplayer/models.py
+++ musicplayer/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Song(models.Model):
     song_title = models.CharField(max_length=200)
-    song_artist = models.CharField(max_length=200)
-    song_duration = models.IntegerField(default=0)
-    cover_image = models.ImageField(upload_to='media')
-    song_file = models.FileField(upload_to='media')
+    song_artist = models.CharField(max_length=200, default='')
+    song_duration = models.DurationField(default='')
+    song_file = models.FileField(upload_to='media', default=False)
+    cover_image = models.ImageField(upload_to='media', default=False)
 
     def __str__(self):
         return self.song_title
