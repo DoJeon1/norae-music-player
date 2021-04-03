015f89efb0a1c08179fd85edc24692dad8c60e0d
DoJeon1 <donghwan.dj@gmail.com>
2021-04-02 23:14:15 -0600

    updated media root for songs and cover images)

--- musicplayer/models.py
+++ musicplayer/models.py
@@ -5,8 +5,8 @@ class Song(models.Model):
     song_title = models.CharField(max_length=200)
     song_artist = models.CharField(max_length=200, default='')
     song_duration = models.DurationField(default='')
-    song_file = models.FileField(upload_to='media', default=False)
-    cover_image = models.ImageField(upload_to='media', default=False)
+    song_file = models.FileField()
+    cover_image = models.ImageField()
     paginate_by = 2
 
     def __str__(self):
